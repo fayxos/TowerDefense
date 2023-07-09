b342553c945f51589151ad9d5aec92fa0f550a9b
YannicFlad <smokespickey@gmail.com>
2023-07-09 21:48:46 +0200

    shop GUI fertig

--- TowerDefense/src/game/Main/GameShop.java
+++ TowerDefense/src/game/Main/GameShop.java
@@ -9,7 +9,7 @@ import game.Structures.Building;
 
 public class GameShop {
 
-	static Image shopCardImage;
+	static Image shopCardImage, goldImage;
 	static Building archerTower;
 	
 	static final int width = 226;
@@ -21,6 +21,7 @@ public class GameShop {
 	
     public static void start() {
 		shopCardImage = loadImage("assets/FantasyWoodenPack/UI board Medium  parchment.png", width, height);
+		goldImage = GameState.loadImage("assets/gold.png", 40, 40);
 		archerTower = new ArcherTower();
 	}
 	
@@ -31,21 +32,33 @@ public class GameShop {
 	}
 	
 	public static void draw(Graphics graphics) {
+		graphics.setFont(new Font("TimesRoman", Font.BOLD, 30));
 		AffineTransform trans = new AffineTransform();
 		trans.rotate(Math.PI/2);
 
 		trans.translate(yStart,-(xStart+height));
 		((Graphics2D)graphics).drawImage(shopCardImage, trans, null);
 		archerTower.draw(graphics, xStart + height/2, yStart + width/2);
+		graphics.drawImage(goldImage, xStart + (int)(height*0.3) - 20, yStart + (int)(width*0.8) - 30, null);
+		graphics.drawString(Integer.toString(20), xStart + (int)(height*0.45), yStart + (int)(width*0.8));
+		
 		trans.translate(0,-xCardOffset);
 		((Graphics2D)graphics).drawImage(shopCardImage, trans, null);
 		archerTower.draw(graphics, xStart + height/2 + xCardOffset, yStart + width/2);
+		graphics.drawImage(goldImage, xStart + (int)(height*0.3) - 20 + xCardOffset, yStart + (int)(width*0.8) - 30, null);
+		graphics.drawString(Integer.toString(50), xStart + (int)(height*0.45) + xCardOffset, yStart + (int)(width*0.8));
+		
 		trans.translate(0,-xCardOffset);
 		((Graphics2D)graphics).drawImage(shopCardImage, trans, null);
 		archerTower.draw(graphics, xStart + height/2 + xCardOffset*2, yStart + width/2);
+		graphics.drawImage(goldImage, xStart + (int)(height*0.3) - 20 + xCardOffset*2, yStart + (int)(width*0.8) - 30, null);
+		graphics.drawString(Integer.toString(300), xStart + (int)(height*0.45) + xCardOffset*2, yStart + (int)(width*0.8));
+		
 		trans.translate(0,-xCardOffset);
 		((Graphics2D)graphics).drawImage(shopCardImage, trans, null);
 		archerTower.draw(graphics, xStart + height/2 + xCardOffset*3, yStart + width/2);
+		graphics.drawImage(goldImage, xStart + (int)(height*0.3) - 20 + xCardOffset*3, yStart + (int)(width*0.8) - 30, null);
+		graphics.drawString(Integer.toString(1000), xStart + (int)(height*0.45) + xCardOffset*3, yStart + (int)(width*0.8));
 	}
 
 }
