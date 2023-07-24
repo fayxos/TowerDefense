4c8f8dcf8e4760e47926084dccd849ca17980179
YannicFlad <98184993+YannicFlad@users.noreply.github.com>
2023-07-24 19:12:14 +0200

    .

--- TowerDefense/src/game/Main/GameShop.java
+++ TowerDefense/src/game/Main/GameShop.java
@@ -6,23 +6,18 @@ import java.awt.geom.AffineTransform;
 import game.Structures.ActiveBuilding;
 import game.Structures.ArcherTower;
 import game.Structures.Building;
+import game.Structures.Structure;
 
 public class GameShop {
 
 	static Image shopCardImage, goldImage;
-	static Building archerTower;
-	
-	static final int width = 226;
-	static final int height = 158;
 	
 	static final int xStart = 15;
 	static final int yStart = 660;
-	static final int xCardOffset = 15 + height;
 	
     public static void start() {
 		shopCardImage = loadImage("assets/FantasyWoodenPack/UI board Medium  parchment.png", width, height);
 		goldImage = GameState.loadImage("assets/gold.png", 40, 40);
-		archerTower = new ArcherTower();
 	}
 	
 	public static Image loadImage(String imagePath, int width, int height) {
@@ -31,11 +26,8 @@ public class GameShop {
         return i.getScaledInstance(width, height, Image.SCALE_SMOOTH);
 	}
 	
-	public static void draw(Graphics graphics) {
-		graphics.setFont(new Font("TimesRoman", Font.BOLD, 30));
-		AffineTransform trans = new AffineTransform();
-		trans.rotate(Math.PI/2);
-
+	public void draw(Graphics graphics) {
+		
 		trans.translate(yStart,-(xStart+height));
 		((Graphics2D)graphics).drawImage(shopCardImage, trans, null);
 		archerTower.draw(graphics, xStart + height/2, yStart + width/2);
@@ -59,6 +51,43 @@ public class GameShop {
 		archerTower.draw(graphics, xStart + height/2 + xCardOffset*3, yStart + width/2);
 		graphics.drawImage(goldImage, xStart + (int)(height*0.3) - 20 + xCardOffset*3, yStart + (int)(width*0.8) - 30, null);
 		graphics.drawString(Integer.toString(1000), xStart + (int)(height*0.45) + xCardOffset*3, yStart + (int)(width*0.8));
+	}
+	
+	private class ShopCard extends GameShop {
+		
+		Structure structure;
+		int x, y;
+		String label;
+		static final int width = 226;
+		static final int height = 158;
+		static final int xCardOffset = 15 + height;
+		private AffineTransform trans;
+		private Font font;
+		
+		ShopCard(int x, int y, Structure structure, int cost) {
+			this(x,y,structure,String.valueOf(cost));
+		}
+		
+		ShopCard(int x, int y, Structure structure, String label) {
+			this.x = x; 
+			this.y = y;
+			this.structure = structure;
+			this.label = label;
+			font = new Font("TimesRoman", Font.BOLD, 30);
+			trans = new AffineTransform();
+			trans.rotate(Math.PI/2);
+		}
+		
+		public void draw(Graphics graphics) {
+			graphics.setFont(font);
+			
+			trans.translate(y,-(x+height));
+			((Graphics2D)graphics).drawImage(shopCardImage, trans, null);
+			structure.draw(graphics, x + height/2, y + width/2);
+			graphics.drawImage(goldImage, x + (int)(height*0.3) - 20, y + (int)(width*0.8) - 30, null);
+			graphics.drawString(Integer.toString(20), x + (int)(height*0.45), y + (int)(width*0.8));
+			
+		}
 	}
 
 }
